ef37a02450a75b85948e754b914b2d5c8c3aa69d
Kenia Castro <keniad.ctorres@gmail.com>
2024-11-07 22:07:11 -0700

    termino de clase blade

--- blade/resources/views/_components/card.blade.php
+++ blade/resources/views/_components/card.blade.php
@@ -1,5 +1,5 @@
 <div style="border: 1px solid black; border-radius: 5px; padding: 10px; margin: 5px;">
     <h3>{{ $title }}</h3>
-    <img src="assets/img/wall3.jpg" alt="example" width="128px">
+    <img src="{{ asset('assets/img/wall3.jpg') }}" alt="example" width="128px">
     <p>{{ $content }}</p>
 </div>
--- blade/resources/views/layouts/landing.blade.php
+++ blade/resources/views/layouts/landing.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    @yield('styles')
     <title>
         @yield('title')
     </title>
@@ -11,5 +13,6 @@
 <body>
     @include('layouts._partials.menu')
     @yield('content')
+    @@yield('scripts')
 </body>
 </html>
